ff6b286cb0366116773f9c72a00e56b6d1d0f0ca
Your Name <name@example.com>
2017-01-10 16:43:12 +0700

    Flash Cards 1 Done

--- flashcards.js
+++ flashcards.js
@@ -1,8 +1,54 @@
 "use strict"
 // write your code here
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 var jsonfile = require('jsonfile')
 var file = 'data.json'
 var database = jsonfile.readFileSync(file)
 
-console.log(database);
+class flashcards {
+    constructor() {
+        this._salah = 0
+        this._nilai = 0
+        this._jalan = 0
+    }
+
+    layout(){
+      rl.question('Welcome to JS Flash Cards. To play, just enter the correct term for each definition. Ready? Go!',(input)=>{
+          if(input == ""){
+            this.run()
+          }else{
+            this.layout()
+          }
+      })
+    }
+    run(){
+        if (this._jalan == database.length) {
+            console.log(`Kamu Mengulang Pertanyaan Sebanyak: ${this._salah}`);
+            console.log(`Soal Selesai, Nilaimu: ${this._nilai}`);
+            rl.close()
+        } else {
+            console.log(database[this._jalan].definition)
+            rl.question("Guess: ", (answer) => {
+                if(answer.toLowerCase()==database[this._jalan].term.toLowerCase()){
+                  this._nilai+=10
+                  this._jalan++
+                  this.run()
+                }else{
+                  this._salah++
+                  console.log("Incorrect! Try again");
+                  this.run()
+                }
+            });
+        }
+    }
+}
+
+var game = new flashcards()
+game.layout()
